e0a37a13a98de6533a3b3b9795dfe5b11ba95019
Mabi19 <mateuszbartnik33@gmail.com>
2026-05-29 16:26:30 +0200

    Add table sorting

---
+++ src/components/SortButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="raw sort-button">
+        <svg viewBox="0 0 16 16" width="16" height="16">
+            <path d="M 8 0 L 14 6 L 2 6 Z" stroke-linejoin="round" v-if="up" />
+            <path d="M 8 16 L 14 10 L 2 10 Z" stroke-linejoin="round" v-if="down" />
+        </svg>
+    </button>
+</template>
+
+<script setup lang="ts">
+const { ascending: up, descending: down } = defineProps<{
+    ascending: boolean;
+    descending: boolean;
+}>();
+</script>
+
+<style scoped>
+.sort-button {
+    background: none;
+    border: none;
+
+    &:hover {
+        opacity: 0.9;
+    }
+
+    &:active {
+        opacity: 0.8;
+    }
+}
+</style>
--- src/components/ThePopulationView.vue
+++ src/components/ThePopulationView.vue
@@ -15,16 +15,46 @@ sorting
                 <thead>
                     <tr class="header">
                         <th><!-- expand button --></th>
-                        <th>ID</th>
-                        <th>X</th>
-                        <th>Y</th>
-                        <th>Fitness</th>
+                        <th>
+                            <span>ID</span>
+                            <SortButton
+                                :ascending="sortColumn != 'id' || sortDirection == 'ascending'"
+                                :descending="sortColumn != 'id' || sortDirection == 'descending'"
+                                @click="sortBy('id')"
+                            />
+                        </th>
+                        <th>
+                            <span>X</span>
+                            <SortButton
+                                :ascending="sortColumn != 'x' || sortDirection == 'ascending'"
+                                :descending="sortColumn != 'x' || sortDirection == 'descending'"
+                                @click="sortBy('x')"
+                            />
+                        </th>
+                        <th>
+                            <span>Y</span>
+                            <SortButton
+                                :ascending="sortColumn != 'y' || sortDirection == 'ascending'"
+                                :descending="sortColumn != 'y' || sortDirection == 'descending'"
+                                @click="sortBy('y')"
+                            />
+                        </th>
+                        <th>
+                            <span>Fitness</span>
+                            <SortButton
+                                :ascending="sortColumn != 'fitness' || sortDirection == 'ascending'"
+                                :descending="
+                                    sortColumn != 'fitness' || sortDirection == 'descending'
+                                "
+                                @click="sortBy('fitness')"
+                            />
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
                     <PopulationRow
                         :chromosome="chromosome"
-                        v-for="chromosome of State.population.value"
+                        v-for="chromosome of sortedPopulation"
                         :key="chromosome.id"
                         :class="
                             State.highlightID.value == chromosome.id && hasStrongHighlight
@@ -44,10 +74,44 @@ sorting
 <script setup lang="ts">
 import type { Chromosome } from "@/lib/evolution/chromosome";
 import * as State from "@/state";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import PopulationRow from "./PopulationRow.vue";
+import SortButton from "./SortButton.vue";
 import WindowHeader from "./WindowHeader.vue";
 import WindowHeaderButton from "./WindowHeaderButton.vue";
+
+type SortColumn = "id" | "x" | "y" | "fitness";
+type SortDirection = "ascending" | "descending";
+const sortColumn = ref<SortColumn>("id");
+const sortDirection = ref<SortDirection>("ascending");
+
+const sortedPopulation = computed(() => {
+    return State.population.value.toSorted((a, b) => {
+        const orderMult = sortDirection.value == "descending" ? -1 : 1;
+        const col = sortColumn.value;
+        if (col == "fitness") {
+            const aFit = State.targetFunction.value.eval(a.x, a.y);
+            const bFit = State.targetFunction.value.eval(b.x, b.y);
+            return orderMult * (aFit - bFit);
+        } else if (col == "id") {
+            // All chromosomes in the population list are guaranteed to be of the same generation, so we don't need to compare that.
+            const aId = parseInt(a.id.split("#")[1]!);
+            const bId = parseInt(b.id.split("#")[1]!);
+            return orderMult * (aId - bId);
+        } else {
+            return orderMult * (a[col] - b[col]);
+        }
+    });
+});
+
+function sortBy(col: SortColumn) {
+    if (sortColumn.value == col) {
+        sortDirection.value = sortDirection.value == "ascending" ? "descending" : "ascending";
+    } else {
+        sortColumn.value = col;
+        sortDirection.value = "ascending";
+    }
+}
 
 // If this is true, an entry was clicked, and hovering does nothing.
 const hasStrongHighlight = ref(false);
@@ -125,6 +189,10 @@ function strongHighlight(chromosome: Chromosome) {
 
     .chromosome-extra {
         background-color: var(--bg-dimmer);
+    }
+
+    th button {
+        vertical-align: middle;
     }
 
     tr:nth-child(even of .chromosome) {
